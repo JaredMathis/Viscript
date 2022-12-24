669cd475c32bd7dfe51505b89097859dd1130ef3
JaredMathis <jared.mathis@native.consulting>
2022-12-23 22:38:36 -0500

    progress

--- src/compile_all.mjs
+++ src/compile_all.mjs
@@ -1,6 +1,25 @@
-import { file_json_open } from "./common.mjs";
+import { assert, file_json_open, identifier_is } from "./common.mjs";
 import { compile } from "./compile.mjs";
 
-let parsed = await file_json_open('./src/add.json')
-let compiled = compile(parsed);
-console.log(compiled);
+let files = ['add', 'number_1', , 'number_2', , 'number_3'];
+let parseds = [];
+for (let f of files) {
+    let parsed = await file_json_open(`./src/${f}.json`);
+    parseds.push(parsed);
+    let compiled = compile(parsed);
+    eval(compiled);
+}
+
+for (let parsed of parseds) {
+    for (let test of parsed.tests) {
+        assert(test.type === 'test');
+        let vars = {};
+        for (let name in test.inputs) {
+            let input = test.inputs[name];
+            assert(input.type === 'test_variable');
+            assert(identifier_is(input.name));
+            let value_get = eval(input.name)
+            vars[name] = value_get();
+        }
+    }
+}
