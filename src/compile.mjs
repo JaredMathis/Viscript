f2a56ba95dee0f07a30314c61c2db9b0dda1f5e4
JaredMathis <jared.mathis@native.consulting>
2022-12-23 22:14:46 -0500

    progress

--- src/compile.mjs
+++ src/compile.mjs
@@ -10,8 +10,9 @@ function compile_function(parsed) {
     assert(identifier_is(parsed.name));
 
     return `
-function ${parsed.name}() {
-
+function ${parsed.name}(vars) {
+    let ${parsed.variables.map(v => v.name).join(", ")};
+    ${parsed.variables.filter(v => v['input']).map(v => v.name).map(v => `${v} = vars.${v}`).join(", ")};
 }`
 }
 
