4abc49c01fef91471fc733a49050b5212230b3ef
JaredMathis <jared.mathis@native.consulting>
2022-12-24 00:39:04 -0500

    progress

--- src/compile.mjs
+++ src/compile.mjs
@@ -1,4 +1,4 @@
-import { array_is, assert, digit_is, identifier_is, letters_all_generic, string_is } from "./common.mjs";
+import { array_is, assert, digit_is, eval_global, identifier_is, letters_all_generic, list_single, string_is } from "./common.mjs";
 
 export function compile(parsed) {
     return compile_function(parsed);
@@ -54,5 +54,43 @@ function compile_string(root) {
 
 function compile_code(root) {
     return root.value;
+}
+
+export async function load(file_get) {
+    let files = ['add', 'number_1', 'number_2', 'number_3', "ui_main"];
+    let parseds = [];
+    for (let f of files) {
+        let parsed = await file_get(f);
+        parseds.push(parsed);
+        let compiled = compile(parsed);
+        console.log(compiled);
+        eval_global(compiled);
+    }
+    for (let parsed of parseds) {
+        for (let test of parsed.tests) {
+            assert(test.type === 'test');
+            let test_vars = {};
+            for (let name in test.inputs) {
+                let input = test.inputs[name];
+                assert(input.type === 'test_variable');
+                assert(identifier_is(input.name));
+                let value_get = eval_global(input.name);
+                let input_vars = {};
+                value_get(input_vars);
+                test_vars[name] = input_vars[list_single(Object.keys(input_vars))];
+            }
+            let test_function = eval_global(parsed.name);
+            await test_function(test_vars);
+            for (let name in test.outputs) {
+                let output = test.outputs[name];
+                assert(output.type === 'test_variable');
+                assert(identifier_is(output.name));
+                let value_get = eval_global(output.name);
+                let output_vars = {};
+                value_get(output_vars);
+                assert(test_vars[name] === output_vars[list_single(Object.keys(output_vars))]);
+            }
+        }
+    }
 }
 
--- src/compile_all.mjs
+++ src/compile_all.mjs
@@ -1,38 +1,4 @@
-import { assert, eval_global, file_json_open, identifier_is, list_single } from "./common.mjs";
-import { compile } from "./compile.mjs";
+import { file_json_open } from "./common.mjs";
+import { load } from "./compile.mjs";
 
-let files = ['add', 'number_1', 'number_2', 'number_3', "ui_main"];
-let parseds = [];
-for (let f of files) {
-    let parsed = await file_json_open(`./src/${f}.json`);
-    parseds.push(parsed);
-    let compiled = compile(parsed);
-    console.log(compiled)
-    eval_global(compiled);
-}
-for (let parsed of parseds) {
-    for (let test of parsed.tests) {
-        assert(test.type === 'test');
-        let test_vars = {};
-        for (let name in test.inputs) {
-            let input = test.inputs[name];
-            assert(input.type === 'test_variable');
-            assert(identifier_is(input.name));
-            let value_get = eval_global(input.name);
-            let input_vars = {};
-            value_get(input_vars);
-            test_vars[name] = input_vars[list_single(Object.keys(input_vars))];
-        }
-        let test_function = eval_global(parsed.name);
-        await test_function(test_vars);
-        for (let name in test.outputs) {
-            let output = test.outputs[name];
-            assert(output.type === 'test_variable');
-            assert(identifier_is(output.name));
-            let value_get = eval_global(output.name);
-            let output_vars = {};
-            value_get(output_vars);
-            assert(test_vars[name] === output_vars[list_single(Object.keys(output_vars))])
-        }
-    }
-}
+await load(f => file_json_open(`./src/${f}.json`));
