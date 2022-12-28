dc6a235c14675e91d8c0a3dfcb8a4f206a39fc77
JaredMathis <jared.mathis@native.consulting>
2022-12-28 01:24:32 -0500

    refactor

--- src/compile.mjs
+++ src/compile.mjs
@@ -61,15 +61,50 @@ function compile_code(root) {
 }
 
 export async function load(file_get) {
+    let parseds = await files_all_get(file_get);
+    for (let parsed of parseds) {
+        let compiled = compile(parsed);
+        console.log(compiled);
+        eval_global(compiled);
+    }
+    for (let parsed of parseds) {
+        for (let test of parsed.tests || []) {
+            assert(test.type === 'test');
+            let test_vars = {};
+            for (let name in test.inputs) {
+                let input = test.inputs[name];
+                assert(input.type === 'test_variable');
+                assert(identifier_is(input.name));
+                let value_get = eval_global(input.name);
+                let input_vars = {};
+                value_get(input_vars);
+                test_vars[name] = input_vars[list_single(Object.keys(input_vars))];
+            }
+            let test_function = eval_global(parsed.name);
+            await test_function(test_vars);
+            for (let name in test.outputs) {
+                let output = test.outputs[name];
+                assert(output.type === 'test_variable');
+                assert(identifier_is(output.name));
+                let value_get = eval_global(output.name);
+                let output_vars = {};
+                value_get(output_vars);
+                assert(test_vars[name] === output_vars[list_single(Object.keys(output_vars))]);
+            }
+        }
+    }
+}
+
+async function files_all_get(file_get) {
     let files = [
-        'add', 
-        'number_1', 
-        'number_2', 
+        'add',
+        'number_1',
+        'number_2',
         'number_3',
         'if_else',
         'for_each',
         'list_add',
-        'list_new', 
+        'list_new',
         "ui_main",
         "ui_document_body_get",
         "ui_element_clear",
@@ -103,36 +138,6 @@ export async function load(file_get) {
         let parsed = await file_get(f);
         parseds.push(parsed);
     }
-    for (let parsed of parseds) {
-        let compiled = compile(parsed);
-        console.log(compiled);
-        eval_global(compiled);
-    }
-    for (let parsed of parseds) {
-        for (let test of parsed.tests || []) {
-            assert(test.type === 'test');
-            let test_vars = {};
-            for (let name in test.inputs) {
-                let input = test.inputs[name];
-                assert(input.type === 'test_variable');
-                assert(identifier_is(input.name));
-                let value_get = eval_global(input.name);
-                let input_vars = {};
-                value_get(input_vars);
-                test_vars[name] = input_vars[list_single(Object.keys(input_vars))];
-            }
-            let test_function = eval_global(parsed.name);
-            await test_function(test_vars);
-            for (let name in test.outputs) {
-                let output = test.outputs[name];
-                assert(output.type === 'test_variable');
-                assert(identifier_is(output.name));
-                let value_get = eval_global(output.name);
-                let output_vars = {};
-                value_get(output_vars);
-                assert(test_vars[name] === output_vars[list_single(Object.keys(output_vars))]);
-            }
-        }
-    }
+    return parseds;
 }
 
